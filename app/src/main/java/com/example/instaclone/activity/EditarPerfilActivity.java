0f46c615004e091b79d6355cfa64e37286c94436
Gustavo Souza <gtssouza94@gmail.com>
2020-08-23 19:05:41 -0300

    Alterando imagem de perfil

--- app/src/main/java/com/example/instaclone/activity/EditarPerfilActivity.java
+++ app/src/main/java/com/example/instaclone/activity/EditarPerfilActivity.java
@@ -1,9 +1,16 @@
 package com.example.instaclone.activity;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.Manifest;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,11 +18,18 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.instaclone.R;
+import com.example.instaclone.helper.ConfiguracaoFirebase;
+import com.example.instaclone.helper.Permissao;
 import com.example.instaclone.helper.UsuarioFirebase;
 import com.example.instaclone.model.Usuario;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -27,6 +41,13 @@ public class EditarPerfilActivity extends AppCompatActivity {
     private TextView txtAlterarFoto;
     private StorageReference storageReference;
     private Usuario usuarioLogado;
+    private static final int SELECAO_GALERIA=200;
+    private StorageReference storageRef;
+
+    private String[] permissioesNecessarias = new String[]{
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            //Manifest.permission.CAMERA
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,11 +56,15 @@ public class EditarPerfilActivity extends AppCompatActivity {
 
         usuarioLogado = UsuarioFirebase.getDadosUsuarioLogado();
 
-        imgPerfEdit = findViewById(R.id.circleImgEdit);
-        txtNomeEdit = findViewById(R.id.editTxtAlterarNome);
-        txtEmailEdit = findViewById(R.id.editTxtEmailEdit);
+        imgPerfEdit    = findViewById(R.id.circleImgEdit);
+        txtNomeEdit    = findViewById(R.id.editTxtAlterarNome);
+        txtEmailEdit   = findViewById(R.id.editTxtEmailEdit);
         btnAlterarNome = findViewById(R.id.btnAlterarEditPerfil);
         txtAlterarFoto = findViewById(R.id.btnAlterarFotoEdit);
+
+        storageRef = ConfiguracaoFirebase.getStorageReference();
+
+        Permissao.validarPermissoes(permissioesNecessarias,this,1);
 
         final FirebaseUser usuario = UsuarioFirebase.getUserAtual();
 
@@ -62,8 +87,63 @@ public class EditarPerfilActivity extends AppCompatActivity {
             }
         });
 
+        //Alterar foto do usuario
+        txtAlterarFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                if(i.resolveActivity(getPackageManager()) != null){
+                    startActivityForResult(i, SELECAO_GALERIA);
+                }
+            }
+        });
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close_24);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK){
+            Bitmap imagem = null;
+            try{
+                switch (requestCode){
+                    case SELECAO_GALERIA:
+                        Uri localImagemSelecionada = data.getData();
+                        imagem = MediaStore.Images.Media.getBitmap(getContentResolver(),localImagemSelecionada);
+                        break;
+                }
+
+                if(imagem != null){
+                    //Configura imagem na tela
+                    imgPerfEdit.setImageBitmap(imagem);
+
+                    //Recuperar dados da imagem para o firebase
+                    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                    imagem.compress(Bitmap.CompressFormat.JPEG, 70, byteArrayOutputStream);
+                    byte[] dadosImagem = byteArrayOutputStream.toByteArray();
+                    //Salvar imagem no firebase
+                    StorageReference imageRef = storageRef.child("imagens").child("perfil").child("<id-usuario>.jpeg");
+                    UploadTask uploadTask = imageRef.putBytes(dadosImagem);
+                    uploadTask.addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                       Toast.makeText(EditarPerfilActivity.this, "Erro ao fazer upload da imagem", Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) { Toast.makeText(EditarPerfilActivity.this, "Sucesso ao fazer upload da imagem", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }else {
+
+        }
     }
 
     @Override
