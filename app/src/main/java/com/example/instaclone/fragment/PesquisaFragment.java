04d4dcdc8cb407b63fb3a83a88060fa2b5b1d1dc
Gustavo Souza <gtssouza94@gmail.com>
2020-08-24 10:05:45 -0300

    Criando tela de pesquisa e implementando SearchView

--- app/src/main/java/com/example/instaclone/fragment/PesquisaFragment.java
+++ app/src/main/java/com/example/instaclone/fragment/PesquisaFragment.java
@@ -3,24 +3,23 @@ package com.example.instaclone.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
 import com.example.instaclone.R;
 
 public class PesquisaFragment extends Fragment {
 
-    public PesquisaFragment() {
-        // Required empty public constructor
-    }
+    private SearchView searchViewPesquisa;
+    private RecyclerView recyclerViewPesquisa;
 
-    public static PesquisaFragment newInstance(String param1, String param2) {
-        PesquisaFragment fragment = new PesquisaFragment();
-        Bundle args = new Bundle();
-        fragment.setArguments(args);
-        return fragment;
+    public PesquisaFragment() {
+
     }
 
     @Override
@@ -29,8 +28,28 @@ public class PesquisaFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_pesquisa, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+       View view = inflater.inflate(R.layout.fragment_pesquisa, container, false);
+
+       searchViewPesquisa = view.findViewById(R.id.searchViewPesquisa);
+       recyclerViewPesquisa = view.findViewById(R.id.recyclerViewPesquisa);
+
+       //Configura searchView
+        searchViewPesquisa.setQueryHint("Buscar usuários");
+        searchViewPesquisa.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Log.d("onQueryTextSubmit","texto digitado" + query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Log.d("onQueryTextChange","texto digitado" + newText);
+                return true;
+            }
+        });
+
+       return view;
     }
 }
