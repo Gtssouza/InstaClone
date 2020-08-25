918ac25e834221ae18d4e664509cc21978ec2320
Gustavo Souza <gtssouza94@gmail.com>
2020-08-25 15:22:50 -0300

    Classe RecyclerItemClickListener adicionado

--- app/src/main/java/com/example/instaclone/helper/Base64Custom.java
+++ app/src/main/java/com/example/instaclone/helper/Base64Custom.java
@@ -5,7 +5,7 @@ import android.util.Base64;
 public class Base64Custom {
 
     public static  String codificaBase64(String txt){
-        return Base64.encodeToString(txt.getBytes(), Base64.DEFAULT).replaceAll("(\\n|\\r)", "");
+        return Base64.encodeToString(txt.getBytes(), Base64.DEFAULT).replaceAll("(\\n | \\r)", "");
     }
 
     public static String decodificaBase64(String txtDecod){
