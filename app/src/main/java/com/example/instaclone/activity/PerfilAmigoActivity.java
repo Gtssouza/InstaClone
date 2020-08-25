bc3dbc072a10947af8504c4e1e30f3359373ff15
Gustavo Souza <gtssouza94@gmail.com>
2020-08-25 16:22:03 -0300

    Alterando botão editar para seguir e exibindo nome do usuário

--- app/src/main/java/com/example/instaclone/activity/PerfilAmigoActivity.java
+++ app/src/main/java/com/example/instaclone/activity/PerfilAmigoActivity.java
@@ -1,16 +1,46 @@
 package com.example.instaclone.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.instaclone.R;
+import com.example.instaclone.model.Usuario;
+
+import java.io.Serializable;
 
 public class PerfilAmigoActivity extends AppCompatActivity {
+    private Usuario usuarioSelecionado;
+    private Button buttonAcaoPerfil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_amigo);
+
+        buttonAcaoPerfil = findViewById(R.id.btnEditarPerfil);
+        buttonAcaoPerfil.setText("Seguir");
+
+        Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
+        toolbar.setTitle("Perfil");
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close_24);
+
+        //Recupera usuario selecionado
+        Bundle bundle = getIntent().getExtras();
+        if(bundle != null){
+            usuarioSelecionado = (Usuario) bundle.getSerializable("usuarioSelecionado");
+            getSupportActionBar().setTitle(usuarioSelecionado.getNome());
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return false;
     }
 }
